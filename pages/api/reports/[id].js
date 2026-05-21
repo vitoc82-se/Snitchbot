5d95f6b9e8aff40fef5810d41ad065a33c69d1fb
vitoc82-se <skanova@gmail.com>
2026-05-21 19:08:15 +0200

    Deploy preview

--- pages/api/reports/[id].js
+++ pages/api/reports/[id].js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import authOptions from '../auth/[...nextauth]';
+import { authOptions } from '../auth/[...nextauth]';
 import sql from '../../../lib/db';
 
 export default async function handler(req, res) {
